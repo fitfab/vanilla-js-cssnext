05d6570cbd4fbf8fc8c2bc5d4e4648966f6e1be5
Miguel Julio <mjulio@artnet.com>
2017-10-12 14:58:14 -0400

    add eslint

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,18 @@
+
 import '../css/login.css'
 
-console.log('voila')
+const Manage = (config) => {
+    const AppView = document.getElementById('app')
+    const disclaimer = document.createElement('p')
+    disclaimer.innerHTML = `This is ${config}`
+    AppView.appendChild(disclaimer)
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        // execute app when dom is ready
+        Manage(document.readyState)
+    }
+}
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,6 @@ module.exports = {
                 }]
             }
         ]
-    }
+    },
+    watch: true
 }
