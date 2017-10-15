7b6c81195ea6857ced6e692ca87be73fc5e2e2ab
Miguel Julio <fitfab@yahoo.com>
2017-10-15 18:07:45 -0400

    fix auto fill label

--- src/js/auto-fill-fix.js
+++ src/js/auto-fill-fix.js
@@ -1,34 +1,11 @@
 const FixAutoFil = () => {
-    const AUTOFILLED = 'filled'
-    const autoComplete = document.querySelectorAll('input:-webkit-autofill')
-    console.log(autoComplete.length)
-    autoComplete.forEach( input => {
-        input.focus()
-        setTimeout( () => {
-            input.blur()
-        }, 300)
-    })
-    //autoComplete[autoComplete.length].focus()
-    // setTimeout( () => {
-    //     document.querySelectorAll('input:-webkit-autofill').forEach( input => {
-    //         console.log(input)
-    //         input.click()
-    //         input.blur()
-    //     })
-    // }, 300)
-
-    // document.addEventListener('input', function(e) {
-    //     console.log(e.target)
-    //     var element = e.target.matches(':-webkit-autofill')
-    //     if(element) {
-    //         // Field auto-completed - Send an analytics event (or whatnot)
-    //         console.log(element)
-    //         e.target.classList.add(AUTOFILLED)
-    //     }
-    //     e.target.focus()
-    //     e.target.blur()
-    //     e.target.click()
-    // })
+    const AUTOFILLED = 'filled'    
+    setTimeout( () => {
+        document.querySelectorAll('input:-webkit-autofill').forEach( input => {
+            input.classList.add(AUTOFILLED)
+        })
+       
+    }, 100)
 }
 
 export default FixAutoFil
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,5 @@ module.exports = {
                 }]
             }
         ]
-    },
-    watch: true
+    }
 }
